4b5ce6c455de9b2203867a860b46a2e84ce9924f
Nick Strong <nstrong92@yahoo.com>
2022-01-06 12:16:14 -0700

    created test content in App.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+// first parameter is the entire app, second argument points to root div (public/index.html)
+ReactDOM.render(<App />, document.getElementById("root"));
